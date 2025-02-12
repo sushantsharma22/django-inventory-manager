03866b3ffcf16080d68b3367145b665e5aaf737a
sushant-sharma <ssewuna123@gmail.com>
2025-02-12 12:49:10 -0500

    added updates

--- lab4/urls.py
+++ lab4/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),# Include `myapp` URLs
+    path('myapp/', include('myapp.urls')),# Include `myapp` URLs
 ]
 
 
