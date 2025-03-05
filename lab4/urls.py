9d6e5452595e83009a9bcd5773b37ac2cd6adc00
sushant-sharma <ssewuna123@gmail.com>
2025-03-05 11:46:04 -0500

    updates

--- lab4/urls.py
+++ lab4/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from myapp import views
+
 
 urlpatterns = [
+    path('', views.welcome, name='welcome'),
     path('admin/', admin.site.urls),
     path('myapp/', include('myapp.urls')),# Include `myapp` URLs
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,54 +1,27 @@
 # Import necessary classes and functions
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from .models import Category, Product
 
+from django.shortcuts import render, get_object_or_404
+from .models import Category
 
 def index(request):
-
+    # Query the first 10 categories ordered by id.
     cat_list = Category.objects.all().order_by('id')[:10]
+    # Pass the context variable 'cat_list' to the template.
+    return render(request, 'myapp/index0.html', {'cat_list': cat_list})
 
-    product_list = Product.objects.all().order_by('-price')[:5]
-
-    response = HttpResponse()
-
-    heading_categories = '<p><strong>List of Categories:</strong></p>'
-    response.write(heading_categories)
-    for category in cat_list:
-        para = f'<p>{category.id}: {category}</p>'
-        response.write(para)
-
-    heading_products = '<p><strong>List of up to 5 Products (Most Expensive First):</strong></p>'
-    response.write(heading_products)
-    for product in product_list:
-        para = f'<p>{product.id}: {product} - ${product.price}</p>'
-        response.write(para)
-
-    return response
-
+# The about view (initial version uses about0.html)
 def about(request):
-    return HttpResponse("This is an Online Store APP.")
+    return render(request, 'myapp/about0.html')
 
+# The detail view for a specific category (initial version uses detail0.html)
 def detail(request, cat_no):
-    # Use get_object_or_404 to return a 404 error if the category does not exist.
-    category = get_object_or_404(Category, id=cat_no)
+    # Retrieve the category with id=cat_no
+    cat = get_object_or_404(Category, id=cat_no)
+    # Assume a related name "products" for the products belonging to the category.
+    product_list = cat.products.all() if hasattr(cat, 'products') else []
+    return render(request, 'myapp/detail0.html', {'cat': cat, 'product_list': product_list})
 
-    # Retrieve the warehouse field
-    warehouse_location = getattr(category, 'warehouse', 'Unknown')
 
-    # Retrieve all products associated with this category.
-    product_list = Product.objects.filter(category=category)
-
-    response = HttpResponse()
-    header = f'<p><strong>Warehouse Location for Category {category.id}:</strong> {warehouse_location}</p>'
-    response.write(header)
-
-    product_header = '<p><strong>List of Products in this Category:</strong></p>'
-    response.write(product_header)
-
-    for product in product_list:
-        para = f'<p>{product.id}: {product} - ${product.price}</p>'
-        response.write(para)
-
-    return response
-
+def welcome(request):
+    return HttpResponse("Welcome to the Online Store!")
